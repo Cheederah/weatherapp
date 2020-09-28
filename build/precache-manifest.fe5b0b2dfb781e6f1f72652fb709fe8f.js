16b375d36ac0d8f20418702af05813738f1a5900
Reebby <rebbyoshiobughie@gmail.com>
2020-09-28 11:55:51 +0100

    adding build file

---
+++ build/precache-manifest.fe5b0b2dfb781e6f1f72652fb709fe8f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7da966469011f4316cfb61ea524f26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42016c864508a153fe4f",
+    "url": "/static/css/main.3adeefb5.chunk.css"
+  },
+  {
+    "revision": "3472541a015cff1d2f88",
+    "url": "/static/js/2.e62d1ad0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e62d1ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42016c864508a153fe4f",
+    "url": "/static/js/main.2509baed.chunk.js"
+  },
+  {
+    "revision": "7eabc36fe0f714c9ecca",
+    "url": "/static/js/runtime-main.7599646d.js"
+  },
+  {
+    "revision": "1653b81251f70836564618cb26c14780",
+    "url": "/static/media/backgound.1653b812.jpg"
+  },
+  {
+    "revision": "0b2a268602d0df3efd2758204f7b3885",
+    "url": "/static/media/cold.0b2a2686.jpg"
+  },
+  {
+    "revision": "2d511e53e9546b5952415ac5f20133fd",
+    "url": "/static/media/hot.2d511e53.jpg"
+  },
+  {
+    "revision": "004c48ca2967078e799e1276e9f20f4b",
+    "url": "/static/media/loader.004c48ca.svg"
+  }
+]);
